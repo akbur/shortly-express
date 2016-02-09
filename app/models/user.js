fe1e519a425b912b4c6abf9b21be4d97c02fd5c7
Kris Burke <kris.burke611@gmail.com>
2016-02-09 13:16:03 -0500

    Created users table.

--- app/models/user.js
+++ app/models/user.js
@@ -3,6 +3,28 @@ var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
 var User = db.Model.extend({
+  tableName: 'users',
+  hasTimeStamps: false,
+
+  initialize: function(){
+    this.on('creating', this.hashedPassword, this);
+  },
+
+  hashedPassword: function(model, attrs, options){
+    return new Promise(function(resolve, reject){
+      console.log('username', model.attributes.username);
+      console.log('password', model.attributes.password);
+      bcrypt.hash(model.attributes.password, 10, null, function(err, hash){
+        if (err) {
+          reject(err);
+        }
+        model.set('password', hash);
+        console.log(1);
+        resolve(hash);
+      });
+    });
+  }
+
 });
 
 module.exports = User;
